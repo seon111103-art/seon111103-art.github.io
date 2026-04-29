8af6d9a9082ef0e0141a5b142415912cf411eace
seon111103-art <seon111103-art@github.com>
2026-04-29 13:21:25 +0900

    서비스워커 캐시 버전 v3으로 업데이트 - 네트워크 우선 전략으로 변경

--- sw.js
+++ sw.js
@@ -1,12 +1,22 @@
-const CACHE = 'gguldanji-v1';
-const ASSETS = ['/', '/index.html', '/character.png', '/manifest.json'];
+const CACHE = 'gguldanji-v3';
+const ASSETS = ['/', '/index.html', '/honey-char.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
